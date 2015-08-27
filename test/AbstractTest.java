1e1d1575625d1a3a71ec881308591ccbb5cff5fd
Ola Sundell <ola.sundell@gmail.com>
2015-08-27 16:32:44 +0200

    Renewed efforts on Teads

--- test/AbstractTest.java
+++ test/AbstractTest.java
@@ -1,11 +1,20 @@
+import org.junit.Assert;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public abstract class AbstractTest {
 	protected final static String DIR = "/Users/olasundell/code/codingame/src/";
 	protected abstract String getDir();
+
+	protected void runTest(int i, Function<Scanner, String> function) throws IOException {
+		String result = function.apply(buildScanner("Test_" + i + "_input.txt"));
+		String expected = readFile("Test_" + i + "_output.txt");
+		Assert.assertEquals(expected, result);
+	}
 
 	protected Scanner buildScanner(String s) throws FileNotFoundException {
 		return new Scanner(new File(DIR + getDir() + s));
--- test/TeadsChallengeSolutionTest.java
+++ test/TeadsChallengeSolutionTest.java
@@ -1,18 +1,75 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TeadsChallengeSolutionTest {
+import java.io.IOException;
+
+public class TeadsChallengeSolutionTest extends AbstractTest {
+//	@Test
+//	public void shouldCalculateDistance() {
+//		TeadsChallengeSolution.Node node = new TeadsChallengeSolution.Node(TeadsChallengeSolution.Node.NO_PARENT);
+//
+////		node.children.add(new Solution.Node(node));
+//		final TeadsChallengeSolution.Node node1 = new TeadsChallengeSolution.Node(node);
+//		node.children.add(node1);
+//		node1.children.add(new TeadsChallengeSolution.Node(node1));
+//		node1.children.add(new TeadsChallengeSolution.Node(node1));
+//
+//		Assert.assertEquals(2, TeadsChallengeSolution.largestDistance(node) - 1);
+//		Assert.assertEquals(1, TeadsChallengeSolution.largestDistance(node1) - 1);
+//	}
+
 	@Test
-	public void shouldCalculateDistance() {
-		TeadsChallengeSolution.Node node = new TeadsChallengeSolution.Node(TeadsChallengeSolution.Node.NO_PARENT);
+	public void one() throws IOException {
+		runTest(1, TeadsChallengeSolution::solve);
+	}
 
-//		node.children.add(new Solution.Node(node));
-		final TeadsChallengeSolution.Node node1 = new TeadsChallengeSolution.Node(node);
-		node.children.add(node1);
-		node1.children.add(new TeadsChallengeSolution.Node(node1));
-		node1.children.add(new TeadsChallengeSolution.Node(node1));
+	@Test
+	public void two() throws IOException {
+		runTest(2, TeadsChallengeSolution::solve);
+	}
 
-		Assert.assertEquals(2, TeadsChallengeSolution.largestDistance(node) - 1);
-		Assert.assertEquals(1, TeadsChallengeSolution.largestDistance(node1) - 1);
+	@Test
+	public void three() throws IOException {
+		runTest(3, TeadsChallengeSolution::solve);
+	}
+
+	@Test
+	public void four() throws IOException {
+		runTest(4, TeadsChallengeSolution::solve);
+	}
+
+	@Test
+	public void five() throws IOException {
+		runTest(5, TeadsChallengeSolution::solve);
+	}
+
+	@Test
+	public void six() throws IOException {
+		runTest(6, TeadsChallengeSolution::solve);
+	}
+
+	@Test
+	public void seven() throws IOException {
+		runTest(7, TeadsChallengeSolution::solve);
+	}
+
+	@Test
+	public void eight() throws IOException {
+		runTest(8, TeadsChallengeSolution::solve);
+	}
+
+	@Test
+	public void nine() throws IOException {
+		runTest(9, TeadsChallengeSolution::solve);
+	}
+
+	@Test
+	public void ten() throws IOException {
+		runTest(10, TeadsChallengeSolution::solve);
+	}
+
+	@Override
+	protected String getDir() {
+		return "teads/";
 	}
 }
